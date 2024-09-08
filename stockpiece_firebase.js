2bc549962556c3a022de46401a078ab95a36b128
KhalidAbdirahman <117296794+KhalidAbdirahman@users.noreply.github.com>
2024-09-08 00:56:59 -0500

    Add files via upload

---
+++ stockpiece_firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARxhxBd47614W-vGKOiMVJqKaFoGLHkjQ",
+  authDomain: "stock-piece.firebaseapp.com",
+  projectId: "stock-piece",
+  storageBucket: "stock-piece.appspot.com",
+  messagingSenderId: "472354999843",
+  appId: "1:472354999843:web:13df9073262f985a976b96",
+  measurementId: "G-WMCBV4QYZR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
